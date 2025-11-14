19146cccb3e1db54a2d9ae27a59ca56c7a825fc5
potzplitz <janschilfahrt@gmail.com>
2025-11-14 19:32:25 +0100

    Routenfix in Suche

--- templates/geometrydash/aredl.php
+++ templates/geometrydash/aredl.php
@@ -1,5 +1,5 @@
 <div class="list">
-    <form id="searchbar" method="POST" action="/gd/aredl">
+    <form id="searchbar" method="POST" action="/geometrydash/extremedemons">
         <div class="search-wrap">
             <button id="check_levels" type="button" class="<-COMPLETED-> submit-aredl-search" style="margin-right: 5px;">
                 <svg viewBox="0 0 24 24" aria-hidden="true" style="color: white;">
@@ -22,18 +22,18 @@
         </span>
     </p>
     <div class="paging d-flex justify-content-center align-items-center gap-2 my-4">
-        <a href="/gd/aredl?page=<-PREV_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_PREV_PAGE->">‹</a>
+        <a href="/geometrydash/extremedemons?page=<-PREV_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_PREV_PAGE->">‹</a>
     <div class="page-numbers">
         <-PAGE_LINKS->
     </div>
-    <a href="/gd/aredl?page=<-NEXT_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_NEXT_PAGE->">›</a>
+    <a href="/geometrydash/extremedemons?page=<-NEXT_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_NEXT_PAGE->">›</a>
     </div>
     <-LIST->
     <div class="paging d-flex justify-content-center align-items-center gap-2 my-4">
-        <a href="/gd/aredl?page=<-PREV_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_PREV_PAGE->">‹</a>
+        <a href="/geometrydash/extremedemons?page=<-PREV_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_PREV_PAGE->">‹</a>
         <div class="page-numbers">
             <-PAGE_LINKS->
         </div>
-        <a href="/gd/aredl?page=<-NEXT_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_NEXT_PAGE->">›</a>
+        <a href="/geometrydash/extremedemons?page=<-NEXT_PAGE->" class="<-DISP_PAGES-> page-link <-DISP_NEXT_PAGE->">›</a>
     </div>
 </div>
