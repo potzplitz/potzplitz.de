514e7f5b1a8cb42fabfa96786166f5f755c06ad4
potzplitz <janschilfahrt@gmail.com>
2025-11-30 02:13:33 +0100

    Bugfixes Nine Circles List

--- javascript/main.js
+++ javascript/main.js
@@ -2,74 +2,74 @@ $(document).ready(function() {
     const varHolder = $('body').data("varHolder");
 });
 
-    class Message {
-        #messagetypes = ["log", "info", "error", "debug"];
-        #type = "";
-        #message = "";
-        #timeout = 3000;
+class Message {
+    #messagetypes = ["log", "info", "error", "debug"];
+    #type = "";
+    #message = "";
+    #timeout = 3000;
 
-        constructor(type) {
-            this.#type = type;
-            this.#validateMessageTypes(type);
-        }
+    constructor(type) {
+        this.#type = type;
+        this.#validateMessageTypes(type);
+    }
 
-        setMessage(message) {
-            this.#message = message;
-        }
+    setMessage(message) {
+        this.#message = message;
+    }
 
-        setHideTimeout(timeout){
-            this.#timeout = timeout;
-        }
+    setHideTimeout(timeout){
+        this.#timeout = timeout;
+    }
 
-        showMessage() {
-            let message = this.#message;
-            const $messagebox = $('#messagebox');
-            let color = this.#getBoxColor(this.#type);
-            this.#validateMessage(message);
+    showMessage() {
+        let message = this.#message;
+        const $messagebox = $('#messagebox');
+        let color = this.#getBoxColor(this.#type);
+        this.#validateMessage(message);
 
-            $messagebox.find('p').text(message);
-            $messagebox.removeClass('errorColor logColor infoColor debugColor').addClass(color);
+        $messagebox.find('p').text(message);
+        $messagebox.removeClass('errorColor logColor infoColor debugColor').addClass(color);
 
-            $messagebox.css({ right: '-300px', opacity: 0 }).show()
-                .animate({ right: '0.5%', opacity: 1 }, 500);
-            
-            setTimeout(() => {
-                $messagebox.animate({ right: '-300px', opacity: 0 }, 500, function() {
-                    $(this).hide();
-                    $(this).removeClass(color);
-                    $(this).find('p').text("");
-                });
-            }, this.#timeout);
-        }
+        $messagebox.css({ right: '-300px', opacity: 0 }).show()
+            .animate({ right: '0.5%', opacity: 1 }, 500);
+        
+        setTimeout(() => {
+            $messagebox.animate({ right: '-300px', opacity: 0 }, 500, function() {
+                $(this).hide();
+                $(this).removeClass(color);
+                $(this).find('p').text("");
+            });
+        }, this.#timeout);
+    }
 
-        #getBoxColor(type) {
-            switch(type) {
-                case "error":
-                    return "errorColor";
+    #getBoxColor(type) {
+        switch(type) {
+            case "error":
+                return "errorColor";
 
-                case "log":
-                    return "logColor";
+            case "log":
+                return "logColor";
 
-                case "info":
-                    return "infoColor";
+            case "info":
+                return "infoColor";
 
-                case "debug":
-                    return "debugColor";
+            case "debug":
+                return "debugColor";
 
-                default:
-                    throw new Error("Unknown Message Type!");
-            }
-        }
-
-        #validateMessageTypes(type) {
-            if(!this.#messagetypes.includes(type)) {
+            default:
                 throw new Error("Unknown Message Type!");
-            }
-        }
-
-        #validateMessage(message) {
-            if(!message) {
-                throw new Error("You have to set a message!");
-            }
         }
     }
+
+    #validateMessageTypes(type) {
+        if(!this.#messagetypes.includes(type)) {
+            throw new Error("Unknown Message Type!");
+        }
+    }
+
+    #validateMessage(message) {
+        if(!message) {
+            throw new Error("You have to set a message!");
+        }
+    }
+}
