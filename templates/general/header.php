ce101ed581175fbcf7d8b17476d60fc63ff50b09
potzplitz <janschilfahrt@gmail.com>
2025-12-27 17:33:21 +0100

    Fix Persistent Session und Mobile Layout

--- templates/general/header.php
+++ templates/general/header.php
@@ -1,19 +1,20 @@
 <div class="header">
   <div class="header-left">
     <a href="/" style="text-decoration: none;">
-      <span id="header"><-TITLE-></span>
+        <span id="header" class="d-block text-truncate fs-6 fs-md-5">
+          <-TITLE->
+      </span>
     </a>
   </div>
-
   <div class="header-right">
     <div class="admin <-DISP_ADMIN->">
       <a href="/admin">
-        <button>Adminpanel</button>
+        <button>Admin</button>
       </a>
     </div>
     <div class="profile-banner">
       <div class="profile-name"><-USERNAME-></div>
-      <img class="profile-pic" src="<-PROFILEPICTURE->" alt="Profil">
+      <img class="profile-pic" src="<-PROFILEPICTURE->" alt="pfp">
     </div>
   </div>
 </div>
